0d5617498555722cd84b484023ce7b290113ace0
Iamdaac <diego.alvarado.1801@gmail.com>
2024-10-27 14:04:26 -0500

    Full Day1 of the course, variables, input functions and string manipulation

---
+++ Day_1/task.py
@@ -0,0 +1,18 @@
+#Write your code below this line
+# print ("Hello world!")
+
+#String Manipulation and Code Intelligence
+# print("Hello" + " " + "Angela\nHow you Doing?")
+
+#Python Input function and variables
+# userName = input("What's your name?\n")
+# print("Nice to meet you", userName +"!")
+#How many characters are in the  string 
+# print(len(userName))
+
+#Challenge 1 Band Name Generator
+
+print("Welcome to the Band Name Generator.\n")
+userCity = input("What's the name of the city you grew up in?\n")
+userPetName = input("What's your pet's name?\n")
+print("Your band name could be", userCity + " " + userPetName)
