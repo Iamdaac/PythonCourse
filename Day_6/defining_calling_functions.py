8fe27700eda309feac5deeca2062c0279d61de6c
Iamdaac <diego.alvarado.1801@gmail.com>
2024-12-01 12:57:57 -0500

    Day_6 finished

---
+++ Day_6/defining_calling_functions.py
@@ -0,0 +1,20 @@
+#Print function
+print("Hello!")
+
+#Len function
+num_char = len("Hello")
+print(num_char)
+
+#Create our own function
+"""
+def function_name():
+    #Do this
+    #Then do this
+    #Finally do this
+"""
+def my_function():
+    print("Hello")
+    print("bye")
+
+#Call the function we create
+my_function()
